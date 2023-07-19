4a9f6f5a64c73bf3096b0a44efe3acfe87cf12a0
neverexist37 <neverexist37@gmail.com>
2023-07-19 04:09:44 +0000

    Copilot second commit

---
+++ number.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  let skills = ['HTML', 'CSS', 'JS', 'ReactJS', 'NodeJS'];
+  let member = {
+    name: 'Nguyen Van A',
+    age: 20,
+    address: 'Ha Noi',
+    skills: skills
+  }
+  return member;
+}
